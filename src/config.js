7421be26ee901ac7207d69b029ea61ff9042ca5f
siddhartha.bhatta@earlystartaustralia.com.au <bsiddhartha25@gmail.com>
2019-08-30 12:17:58 +0800

    fix

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
-const DEV_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com ';
+const DEV_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com';
 // const DEV_URL = 'http://localhost:4000';
 const SOCKET_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com:3001';
 // const SOCKET_URL = 'http://localhost:3001';
