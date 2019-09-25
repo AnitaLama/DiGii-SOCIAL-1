f4be4f1a92883d9eee0c5510a00fd41d8761bc7f
anitalama <anitalama07@gmail.com>
2019-09-25 20:14:39 +0545

    add comment

--- src/Saga/CommentSaga.js
+++ src/Saga/CommentSaga.js
@@ -4,13 +4,13 @@ import { DEV_URL } from '../utils/config';
 
 const URL = `${DEV_URL}/comment`;
 export function* onSubmitComment(action) {
-  // try {
-  //   console.log('saga', action.data);
-  //   const { data } = yield call(axios.post, `${URL}/addComment`, action.data);
-  //   console.log('saga output', data);
-  // } catch (err) {
-  //   console.log(err);
-  // }
+  try {
+    console.log('saga', action.data);
+    const { data } = yield call(axios.post, `${URL}/addComment`, action.data);
+    console.log('saga output', data);
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 export function* onCommentDelete(action) {
