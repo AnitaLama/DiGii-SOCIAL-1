2cd4e9a1c4098b1d724dd46220040f3c67b84614
anitalama <anitalama07@gmail.com>
2019-09-25 20:47:45 +0545

    stable dated 25sep

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,11 +1,11 @@
 // const DEV_URL = 'http://api-prod-socket.vuzmxmfskr.ap-southeast-2.elasticbeanstalk.com';
 // const SOCKET_URL = 'http://api-prod-socket.vuzmxmfskr.ap-southeast-2.elasticbeanstalk.com:3001';
 
-const DEV_URL = 'http://localhost:4000';
-const SOCKET_URL = 'http://localhost:3001';
+// const DEV_URL = 'http://localhost:4000';
+// const SOCKET_URL = 'http://localhost:3001';
 
-// const DEV_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com';
-// const SOCKET_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com:3001';
+const DEV_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com';
+const SOCKET_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com:3001';
 
 const GIPHY_API = 'V2UHRCYLnMJVrn1u1R0YVTq9BWzXOECn';
 
