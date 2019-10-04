7285d609114e4a052b008649e1242b3c3c9cda4e
anitalama <anitalama07@gmail.com>
2019-10-04 09:54:22 +0545

    checkpoint 4th oct

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,10 +1,14 @@
-const DEV_URL = 'http://api-prod-socket.vuzmxmfskr.ap-southeast-2.elasticbeanstalk.com';
+const DEV_URL =
+  'http://api-prod-socket.vuzmxmfskr.ap-southeast-2.elasticbeanstalk.com';
+const SOCKET_URL =
+  'http://api-prod-socket.vuzmxmfskr.ap-southeast-2.elasticbeanstalk.com:3001';
+
 // const DEV_URL = 'http://localhost:4000';
-const SOCKET_URL = 'http://api-prod-socket.vuzmxmfskr.ap-southeast-2.elasticbeanstalk.com:3001';
 // const SOCKET_URL = 'http://localhost:3001';
 
-// const DEV_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com';
-// const SOCKET_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com:3001';
+const DEV_URL = 'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com';
+const SOCKET_URL =
+  'http://api-staging-digii.ap-southeast-2.elasticbeanstalk.com:3001';
 
 const GIPHY_API = 'V2UHRCYLnMJVrn1u1R0YVTq9BWzXOECn';
 
@@ -12,6 +16,4 @@ const GIPHY_API = 'V2UHRCYLnMJVrn1u1R0YVTq9BWzXOECn';
 // const GIPHY_API = 'boF6nEfPaO84xFlP58gMKOwp6T1TG5Sa';
 const strikeCount = 3;
 const strikeRound = 3;
-export {
-  DEV_URL, SOCKET_URL, GIPHY_API, strikeCount, strikeRound
-};
+export { DEV_URL, SOCKET_URL, GIPHY_API, strikeCount, strikeRound };
